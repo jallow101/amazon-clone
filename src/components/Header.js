83d56e41799527e4441bf04abb2b45bbbb4a834b
jallow101 <jallow101@users.noreply.github.com>
2022-10-15 20:15:34 +0000

    alt

--- src/components/Header.js
+++ src/components/Header.js
@@ -14,6 +14,7 @@ const Header = () => {
             height={40}
             objectFit={"contain"}
             className="cursor-pointer"
+            alt='logo'
             />
         </div>
     
